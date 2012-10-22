f2e9584d41ea913177df42f6caa1c9b485c0c81c
matailia <matailia@si9.etu.info-ufr.univ-montp2.fr>
2012-10-22 12:05:00 +0200

    second commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,16 +3,43 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.toto;
+import models.*;
+import java.util.*;
+import play.data.*;
 
 public class Application extends Controller {
 
+	static Form<Task> taskForm = form(Task.class);
+
 	public static Result index() {
-		return ok(index.render("Your new application is ready."));
+		// return ok(index.render("Your new application is ready."));
+		// return ok("Hello world");
+		return redirect(routes.Application.tasks());
 	}
-	
-//	public static Result toto(){
-//		return ok(toto.);
-//		
-//	}
+
+	public static Result toto() {
+		return ok(toto.render("boooooooooooooooooomm!!!"));
+
+	}
+
+	public static Result tasks() {
+		return ok(views.html.index.render(Task.all(), taskForm));
+	}
+
+	public static Result newTask() {
+		Form<Task> filledForm = taskForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.index.render(Task.all(), filledForm));
+		} else {
+			Task.create(filledForm.get());
+			return redirect(routes.Application.tasks());
+		}
+	}
+
+	public static Result deleteTask(Long id) {
+		Task.delete(id);
+		return redirect(routes.Application.tasks());
+	}
 
 }
