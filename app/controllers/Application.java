4f47a928a3a67b8195c0990f2d3f6c14060a2529
matailia <matailia@si10.etu.info-ufr.univ-montp2.fr>
2012-11-12 10:24:01 +0100

    3rd commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,23 @@
 package controllers;
 
+
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
-import views.html.toto;
+import views.*;
 import models.*;
+
 import java.util.*;
+
+import play.mvc.BodyParser;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ObjectNode;
+
+
+
+
 import play.data.*;
 
 public class Application extends Controller {
@@ -18,19 +30,15 @@ public class Application extends Controller {
 		return redirect(routes.Application.tasks());
 	}
 
-	public static Result toto() {
-		return ok(toto.render("boooooooooooooooooomm!!!"));
-
-	}
 
 	public static Result tasks() {
-		return ok(views.html.index.render(Task.all(), taskForm));
+		return ok(views.html.index.render(Task.findAll(), taskForm));
 	}
 
 	public static Result newTask() {
 		Form<Task> filledForm = taskForm.bindFromRequest();
 		if (filledForm.hasErrors()) {
-			return badRequest(views.html.index.render(Task.all(), filledForm));
+			return badRequest(views.html.index.render(Task.findAll(), filledForm));
 		} else {
 			Task.create(filledForm.get());
 			return redirect(routes.Application.tasks());
@@ -40,6 +48,20 @@ public class Application extends Controller {
 	public static Result deleteTask(Long id) {
 		Task.delete(id);
 		return redirect(routes.Application.tasks());
+	}
+	
+	public static Result jsonTask() {
+	  ObjectNode result = Json.newObject();
+	  List<Task> tasks = Task.findAll();
+	  int i =0;
+	  ArrayNode arrayJson = result.putArray("tasks");
+	  for (Task task : tasks) {
+		  i++;
+		  arrayJson.add(Json.toJson(task));
+	}
+	  
+	    return ok(result);	    
+	  
 	}
 
 }
